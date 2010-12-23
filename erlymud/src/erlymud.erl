c86f6519e3a8298451290e4dc917203fd6644c5a
Johan Warlander <johan@snowflake.nu>
2010-12-23 01:35:01 +0000

    Adding 'tell' command, fixed removal of user record at logout

--- erlymud/src/erlymud.erl
+++ erlymud/src/erlymud.erl
@@ -3,6 +3,7 @@
 %% API
 -export([connect/1, login/3, parse/3]).
 
+-record(state, {userid}).
 
 %% ===================================================================
 %% Application Interface
@@ -14,28 +15,73 @@ connect(Socket) ->
   {?MODULE, login, got_user}.
 
 login(Socket, UserName, got_user) ->
-  erlymud_users:add(UserName, self()),
-  gen_tcp:send(Socket, "Logging in..\n\n"),
-  gen_tcp:send(Socket, "You're in the Void. Type 'quit' to leave.\n"),
-  gen_tcp:send(Socket, "> "),
-  {next, {?MODULE, parse, []}}.
+  case erlymud_users:add(UserName, Socket) of
+    ok -> 
+      gen_tcp:send(Socket, "Logging in..\n\n"),
+      gen_tcp:send(Socket, "You're in the Void. Type 'quit' to leave.\n"),
+      gen_tcp:send(Socket, " (hint: you can try 'who' and 'tell' as well..)\n"),
+      gen_tcp:send(Socket, "> "),
+      {next, {?MODULE, parse, #state{userid = UserName}}};
+    {error, user_exists} ->
+      gen_tcp:send(Socket, "User already logged in, pick another.\n\n"),
+      gen_tcp:send(Socket, "User: "),
+      {ok, got_user}
+  end.
 
 parse(Socket, Data, State) ->
   case Data of
+    "" ->
+      gen_tcp:send(Socket, "\n> "),
+      {ok, State};
     "who" ->
       display_users(Socket),
       gen_tcp:send(Socket, "\n> "),
       {ok, State};
     "quit" ->
       gen_tcp:send(Socket, "Goodbye!\n"),
+      erlymud_users:remove(State#state.userid),
       done;
     Text ->
-      gen_tcp:send(Socket, "From within the Void, you hear an echo.. '" ++ Text ++ "'\n"),
-      gen_tcp:send(Socket, "\n> "),
-      {ok, State}
+      case try_parse(Socket, Text, State) of
+        ok ->
+          gen_tcp:send(Socket, "\n> "),
+          {ok, State};
+        {error, no_parse_match} ->
+          gen_tcp:send(Socket, "From within the Void, you hear an echo.. '" ++ Text ++ "'\n"),
+          gen_tcp:send(Socket, "\n> "),
+          {ok, State}
+      end
   end.
 
 %% Private functions
+
+try_parse(Socket, Text, State) ->
+  [Verb | Rest ] = string:tokens(Text, " "),
+  case Verb of
+    "tell" ->
+      try_parse_tell(Socket, Rest, State);
+    _Other ->
+      {error, no_parse_match}
+  end.
+
+try_parse_tell(Socket, [], _State) ->
+  gen_tcp:send(Socket, "Syntax: tell <who> <what>\n");
+try_parse_tell(Socket, [Who | []], _State) ->
+  gen_tcp:send(Socket, "Tell " ++ Who ++ " what?\n");
+try_parse_tell(Socket, [Who | What], State) ->
+  case erlymud_users:get(Who) of
+    {ok, Socket} -> 
+      gen_tcp:send(Socket, "Talking to yourself, huh?\n");
+    {ok, OtherUser} ->
+      MyText = io_lib:format("You tell ~s, \"~s\"~n",
+        [Who, string:join(What, " ")]),
+      gen_tcp:send(Socket, MyText),
+      OtherText = io_lib:format("~s tells you, \"~s\"~n",
+        [State#state.userid, string:join(What, " ")]),
+      gen_tcp:send(OtherUser, OtherText);
+    {error, not_found} ->
+      gen_tcp:send(Socket, "No such user found.\n")
+  end.
 
 display_users(Socket) ->
   {ok, List} = erlymud_users:get(),
--- erlymud/src/erlymud_users.erl
+++ erlymud/src/erlymud_users.erl
@@ -2,7 +2,7 @@
 
 -behaviour(gen_server).
 
--export([start_link/0, add/2, get/0]).
+-export([start_link/0, add/2, get/0, get/1, remove/1]).
 
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
@@ -15,22 +15,44 @@ start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
 
 add(User, Pid) ->
-  gen_server:cast(?SERVER, {add, User, Pid}).
+  gen_server:call(?SERVER, {add, User, Pid}).
 
 get() ->
   gen_server:call(?SERVER, get).
+
+get(Who) ->
+  gen_server:call(?SERVER, {get, Who}).
+
+remove(User) ->
+  gen_server:cast(?SERVER, {remove, User}).
 
 %% --------------------------------------------------------------------------
 
 init([]) ->
   {ok, #state{users = dict:new()}}.
 
+handle_call({add, User, Pid}, _From, State) ->
+  case dict:is_key(User, State#state.users) of
+    true ->
+      {reply, {error, user_exists}, State};
+    false ->
+      Users = dict:store(User, Pid, State#state.users),
+      {reply, ok, State#state{users = Users}}
+  end;
 handle_call(get, _From, State) ->
-  Response = dict:to_list(State#state.users),
-  {reply, {ok, Response}, State}.
+  UserList = dict:to_list(State#state.users),
+  {reply, {ok, UserList}, State};
+handle_call({get, Who}, _From, State) ->
+  Reply = case dict:is_key(Who, State#state.users) of
+            true ->
+              {ok, dict:fetch(Who, State#state.users)};
+            false ->
+              {error, not_found}
+          end,
+  {reply, Reply, State}.
 
-handle_cast({add, User, Pid}, State) ->
-  Users = dict:store(User, Pid, State#state.users),
+handle_cast({remove, User}, State) ->
+  Users = dict:erase(User, State#state.users),
   {noreply, State#state{users = Users}}.
 
 handle_info(_Msg, State) ->
