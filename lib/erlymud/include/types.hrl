76ecd7341d5a59ebe1585d183c880a505f3b4b5a
Johan Warlander <johan@snowflake.nu>
2011-01-23 15:00:43 +0100

    Adding typespecs; em_game

--- lib/erlymud/include/types.hrl
+++ lib/erlymud/include/types.hrl
@@ -1,5 +1,6 @@
 %% Basic types
 -type count()           :: non_neg_integer().
+-type file_path()       :: string().
 -type orddict(Key, Val) :: [{Key, Val}].
 
 %% Network stuff
--- lib/erlymud/src/em_game.erl
+++ lib/erlymud/src/em_game.erl
@@ -25,7 +25,22 @@
 -define(SERVER, ?MODULE).
 
 
-%% API functions
+%% ==========================================================================
+%% Type Specifications
+%% ==========================================================================
+-include("types.hrl").
+-type user() :: {string(), pid()}.
+-type users() :: [user()].
+-type user_name() :: string().
+-type user_pid() :: pid().
+-type user_pred() :: fun((user_pid()) -> true|false).
+
+-type living_pid() :: pid().
+
+
+%% ==========================================================================
+%% API Functions
+%% ==========================================================================
 
 start_link() ->
   gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
@@ -33,34 +48,56 @@ start_link() ->
 start() ->
   gen_server:start({local, ?SERVER}, ?MODULE, [], []).
 
+%% @doc Return path to the data directory.
+-spec data_dir() -> file_path().
 data_dir() ->
   filename:join([code:lib_dir(erlymud), "..", "..", "data"]).
 
+%% @doc Return a list of {Name, UserPid} tuples with all logged in users.
+-spec get_users() -> users().
 get_users() ->
   gen_server:call(?SERVER, get_users).
 
+%% @doc Log in a user to the game. Returns an error if the user is already
+%% logged in.
+-spec login(user_pid()) -> ok | {error, user_exists}.
 login(User) ->
   gen_server:call(?SERVER, {login, User}).
 
+%% @doc Incarnate a living, putting them in the room where they last saved.
+-spec incarnate(living_pid()) -> ok.
 incarnate(Living) ->
   gen_server:call(?SERVER, {incarnate, Living}).
 
+%% @doc Log out the specified user. Returns an error if user isn't logged on.
+-spec logout(user_pid()) -> ok | {error, not_found}.
 logout(User) ->
   gen_server:call(?SERVER, {logout, User}).
 
+%% @doc Lookup a user by name, return the {Name, UserPid} tuple.
+-spec lookup_user(user_name()) -> {ok, user()} | {error, not_found}.
 lookup_user(Name) ->
   gen_server:call(?SERVER, {lookup_user, Name}).
 
+%% @doc Lookup a user by pid, return the {Name, UserPid} tuple.
+-spec lookup_user_pid(user_pid()) -> {ok, user()} | {error, not_found}.
 lookup_user_pid(Pid) ->
   gen_server:call(?SERVER, {lookup_user_pid, Pid}).
 
+%% @doc Print something to all users except the specified UserPid.
+-spec print_except(user_pid(), iolist(), list()) -> ok.
 print_except(User, Format, Args) ->
   gen_server:call(?SERVER, {print_except, User, Format, Args}).
 
+%% @doc Print something to all users that satisfy the predicate fun.
+-spec print_while(user_pred(), iolist(), list()) -> ok.
 print_while(Pred, Format, Args) ->
   gen_server:call(?SERVER, {print_while, Pred, Format, Args}).
 
+
+%% ==========================================================================
 %% gen_server callbacks
+%% ==========================================================================
 
 init([]) ->
   process_flag(trap_exit, true),
@@ -115,10 +152,14 @@ code_change(_OldVsn, State, _Extra) ->
   {ok, State}.
 
 
+%% ==========================================================================
 %% Internal functions
+%% ==========================================================================
 
 %% User Handling
 
+-spec do_login(user_pid(), #state{}) -> 
+        {ok, #state{}}|{{error, user_exists}, #state{}}.
 do_login(User, #state{users=Users}=State) ->
   Name = em_user:get_name(User),
   case lists:keyfind(Name, 1, Users) of
@@ -130,6 +171,7 @@ do_login(User, #state{users=Users}=State) ->
       {ok, State#state{users=[{Name, User}|Users]}}
   end.
 
+-spec do_incarnate(living_pid(), #state{}) -> {ok, #state{}}.
 do_incarnate(Living, State) ->
   Name = em_living:get_name(Living),
   Room = case em_living:get_room(Living) of
@@ -144,8 +186,10 @@ do_incarnate(Living, State) ->
   em_room:print_except(Room, Living, "~s arrives.~n", [Name]),
   {ok, State}.
 
-% Do NOT actually touch the User process here, it might have crashed
-% when we call do_logout()
+%% @doc Log out a user. Do NOT actually touch the User process here, it might 
+%% have crashed when we call do_logout(). Or could it really, since we link?!
+-spec do_logout(user_pid(), #state{}) -> 
+  {ok, #state{}}|{{error, not_found}, #state{}}.
 do_logout(User, #state{users=Users}=State) ->
   case lists:keyfind(User, 2, Users) of
     {Name, User} ->
@@ -156,16 +200,20 @@ do_logout(User, #state{users=Users}=State) ->
       {{error, not_found}, State}
   end.
 
+-spec do_lookup_user_pid(user_pid(), users()) -> 
+        {ok, user()}|{error, not_found}.
 do_lookup_user_pid(Pid, Users) ->
   case lists:keyfind(Pid, 2, Users) of
     false -> {error, not_found};
     UserTuple -> {ok, UserTuple}
   end.
 
+-spec do_print_except(users(), user_pid(), iolist(), list()) -> list().
 do_print_except(Users, User, Format, Args) ->
   Pred = fun(U) -> U =/= User end,
   do_print_while(Users, Pred, Format, Args).
 
+-spec do_print_while(users(), user_pred(), iolist(), list()) -> list().
 do_print_while(Users, Pred, Format, Args) ->
   Pids = [Pid || {_User, Pid} <- Users],
   ToPids = lists:filter(Pred, Pids),
