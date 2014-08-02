8dd3e4d9e0f7417944af794bddaef2840252dc0f
Duncan McGreggor <duncan@cogitat.io>
2014-08-01 20:57:40 -0700

    Added banners.

--- lib/erlymud/src/em_rh_login.erl
+++ lib/erlymud/src/em_rh_login.erl
@@ -21,9 +21,52 @@
 %% API Functions
 %% ==========================================================================
 
+banner1() ->
+"
+MM\"\"\"\"\"\"\"\"`M          dP          M\"\"\"\"\"`'\"\"\"`YM M\"\"MMMMM\"\"M M\"\"\"\"\"\"'YMM
+MM  mmmmmmmM          88          M  mm.  mm.  M M  MMMMM  M M  mmmm. `M
+M`      MMMM 88d888b. 88 dP    dP M  MMM  MMM  M M  MMMMM  M M  MMMMM  M
+MM  MMMMMMMM 88'  `88 88 88    88 M  MMM  MMM  M M  MMMMM  M M  MMMMM  M
+MM  MMMMMMMM 88       88 88.  .88 M  MMM  MMM  M M  `MMM'  M M  MMMM' .M
+MM        .M dP       dP `8888P88 M  MMM  MMM  M Mb       dM M       .MM
+MMMMMMMMMMMM                  .88 MMMMMMMMMMMMMM MMMMMMMMMMM MMMMMMMMMMM
+                          d8888P
+".
+
+banner2() ->
+"
+ _______       _       ______  _     _ _____
+(_______)     | |     |  ___ \\| |   | (____ \\
+ _____    ____| |_   _| | _ | | |   | |_   \\ \\
+|  ___)  / ___) | | | | || || | |   | | |   | |
+| |_____| |   | | |_| | || || | |___| | |__/ /
+|_______)_|   |_|\\__  |_||_||_|\\______|_____/
+                (____/
+".
+
+banner3() ->
+"
+   __     _                      ___
+  /__\\ __| |_   _  /\\/\\  /\\ /\\  /   \\
+ /_\\| '__| | | | |/    \\/ / \\ \\/ /\\ /
+//__| |  | | |_| / /\\/\\ \\ \\_/ / /_//
+\\__/|_|  |_|\\__, \\/    \\/\\___/___,'
+            |___/
+
+".
+
+login_instructions() ->
+"
+*** If you are loging in for the first time, enter the user name
+*** you would like to have for your account (case insensitive).
+".
+
 -spec welcome(em_conn:conn_pid()) -> ok.
 welcome(Conn) ->
-  em_conn:print(Conn, "\nWelcome to ErlyMUD " ++ em_util:get_version() ++ "\n\n"),
+  em_conn:print(Conn, "\nWelcome to:\n"
+    ++ banner1() ++ "\nAn ErlyMUD Server, v"
+    ++ em_util:get_version() ++ "\n\n"
+    ++ login_instructions() ++ "\n\n"),
   em_conn:print(Conn, "Login: ").
 
 %% Got a username, do something with it
