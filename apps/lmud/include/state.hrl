d2a3f1af45588bc201fd3e1c0f48548d6165e068
Duncan McGreggor <duncan@cnbb.games>
2022-07-23 21:06:07 -0500

    Moved spell state into shared record.

--- apps/lmud/include/state.hrl
+++ apps/lmud/include/state.hrl
@@ -1,23 +1,31 @@
 -record(state_character, {
-          name="noname" :: em_character:name_type(),
-          room :: em_room:room_pid(),
-          client,
-          desc="" :: string(),
-          objects=[] :: [em_object:object()]
-        }).
+         name="noname" :: em_character:name_type(),
+         room :: em_room:room_pid(),
+         client :: any(),
+         desc="" :: string(),
+         objects=[] :: [em_object:object()]
+      }).
 
--record(object, {ids=[]::em_object:id_list(),
-                 plurals=[]::em_object:id_list(),
-                 adjs=[]::em_object:adj_list(),
-                 primary_id=""::em_object:id(),
-                 primary_adj=""::em_object:adj(),
-                 short="nondescript thing"::string(),
-                 desc=""::string(),
-                 show_in_room=""::string(),
-                 proper_name=""::string(),
-                 quantity=0::em_object:count(),
-                 is_attached=false::boolean(),
-                 is_plural=false::boolean(),
-                 is_unique=false::boolean(),
-                 template="dummy"::string()
-        }).
+-record(state_speller, {
+         spell_mod :: atom(),
+         spell_state :: any(),
+         caster :: any(),
+         room :: any()
+      }).
+
+-record(object, {
+         ids=[] :: em_object:id_list(),
+         plurals=[] :: em_object:id_list(),
+         adjs=[] :: em_object:adj_list(),
+         primary_id="" :: em_object:id(),
+         primary_adj="" :: em_object:adj(),
+         short="nondescript thing" :: string(),
+         desc="" :: string(),
+         show_in_room="" :: string(),
+         proper_name="" :: string(),
+         quantity=0 :: em_object:count(),
+         is_attached=false :: boolean(),
+         is_plural=false :: boolean(),
+         is_unique=false :: boolean(),
+         template="dummy" :: string()
+      }).
--- apps/lmud/src/em_spell.erl
+++ apps/lmud/src/em_spell.erl
@@ -3,6 +3,8 @@
 %%% @copyright 2010-2011 Johan Warlander
 %%% =========================================================================
 -module(em_spell).
+
+-include("apps/lmud/include/state.hrl").
 -include("apps/lmud/include/types.hrl").
 
 -behaviour(gen_server).
@@ -13,8 +15,6 @@
 %% gen_server exports
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2,
          terminate/2, code_change/3]).
-
--record(state, {spell_mod :: atom(), spell_state :: any()}).
 
 -export([behaviour_info/1]).
 
@@ -40,51 +40,51 @@ start_link(Mod, Args) ->
 %% gen_server callbacks
 %% ==========================================================================
 
--spec init(list()) -> {ok, #state{}} | {ok, #state{}, integer()}.
+-spec init(list()) -> {ok, #state_speller{}} | {ok, #state_speller{}, integer()}.
 init([Mod, Args]) ->
   case Mod:init(Args) of
     {ok, SpellState} ->
-      {ok, #state{spell_mod = Mod, spell_state = SpellState}};
+      {ok, #state_speller{spell_mod = Mod, spell_state = SpellState}};
     {ok, SpellState, Timeout} ->
-      {ok, #state{spell_mod = Mod, spell_state = SpellState}, Timeout}
+      {ok, #state_speller{spell_mod = Mod, spell_state = SpellState}, Timeout}
   end.
 
 %% @doc We don't listen to any calls, just empty the queue.
--spec handle_call(any(), pid(), #state{}) -> {reply, ok, #state{}}.
+-spec handle_call(any(), pid(), #state_speller{}) -> {reply, ok, #state_speller{}}.
 handle_call(_Req, _From, State) ->
   {reply, ok, State}.
 
 %% @doc We don't listen to any casts, just empty the queue.
--spec handle_cast(any(), #state{}) -> {noreply, #state{}}.
+-spec handle_cast(any(), #state_speller{}) -> {noreply, #state_speller{}}.
 handle_cast(Msg, State) ->
-  Mod = State#state.spell_mod,
-  SpellState = State#state.spell_state,
+  Mod = State#state_speller.spell_mod,
+  SpellState = State#state_speller.spell_state,
   case Mod:handle_cast(Msg, SpellState) of
     {noreply, NewSpellState} ->
-      {noreply, State#state{spell_state=NewSpellState}};
+      {noreply, State#state_speller{spell_state=NewSpellState}};
     {stop, Reason, _NewSpellState} ->
       {stop, Reason, State}
   end.
 
 %% @doc Pass other messages on to the spell module.
 handle_info(Info, State) ->
-  Mod = State#state.spell_mod,
-  SpellState = State#state.spell_state,
+  Mod = State#state_speller.spell_mod,
+  SpellState = State#state_speller.spell_state,
   case Mod:handle_info(Info, SpellState) of
     {noreply, NewSpellState} ->
-      {noreply, State#state{spell_state=NewSpellState}};
+      {noreply, State#state_speller{spell_state=NewSpellState}};
     {stop, Reason, _NewSpellState} ->
       {stop, Reason, State}
   end.
 
--spec terminate(any(), #state{}) -> ok.
+-spec terminate(any(), #state_speller{}) -> ok.
 terminate(Reason, State) ->
-  Mod = State#state.spell_mod,
-  SpellState = State#state.spell_state,
+  Mod = State#state_speller.spell_mod,
+  SpellState = State#state_speller.spell_state,
   Mod:terminate(Reason, SpellState),
   ok.
 
--spec code_change(string(), #state{}, any()) -> {ok, #state{}}.
+-spec code_change(string(), #state_speller{}, any()) -> {ok, #state_speller{}}.
 code_change(_Vsn, State, _Extra) ->
   {ok, State}.
 
